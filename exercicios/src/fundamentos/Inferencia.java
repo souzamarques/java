fbd05821ac34779f434ae83f8a39e98a7ab699f0
Lucas Souza <lucassouzzamarques@gmail.com>
2021-06-29 19:08:10 -0300

    Inferência

---
+++ exercicios/src/fundamentos/Inferencia.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class Inferencia {
+	
+	public static void main(String[] args) {
+		// Inferência de Tipo
+		var t = 10;
+		var k = 10.5;
+		
+		System.out.println("Tipo inteiro: " + t);
+		System.out.println("Tipo flutuante: " + k);
+	}
+
+}
